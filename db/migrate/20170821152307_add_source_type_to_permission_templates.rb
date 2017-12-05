bd3feb88f6f80a349eb1dfae1632bc23b7ab27c7
Justin Gondron <jgondron@nd.edu>
2017-12-05 09:51:35 -0500

    #1580: Fixed the problems specific to sqlite. The gist is that sqlite doesn't support add/rename/drop columns within an alter table context, so the adapter is forced to recreate the table anytime a migration performs things like rename_column. In this case, there is a foreign key constraint on the table that we're trying to perform this operation on, so it's unable to recreate the table (hence the FKC exception on DROP TABLE). The only solution is to recreate the dependent table without the FKC, perform the actual migration, then recreate the dependent table again, restoring the FKC. I've made use of the adapter to do a lot of the work, but behind it all this is doing a terrible amount of table copies.

--- db/migrate/20170821152307_add_source_type_to_permission_templates.rb
+++ db/migrate/20170821152307_add_source_type_to_permission_templates.rb
@@ -1,16 +1,44 @@
 class AddSourceTypeToPermissionTemplates < ActiveRecord::Migration[4.2]
-  def change
-    add_column :permission_templates, :source_type, :string
-    add_column :permission_templates, :source_id, :string
+  # Recreate the permission_template_accesses table without the FKC on permission_templates.
+  # This is required since the sqlite adapter will have to drop the permission_templates table
+  # just to perform the upcoming column changes, which will violate the FKC.
+  def sqlite_disable_fkc
+    # Renaming the dependent column in sqlite actually recreates the table, silently dropping the fkc
+    rename_column :permission_template_accesses, :permission_template_id, :permission_template_id_old
+  end
 
+  # Recreate the permission_template_accesses table with the FKC.
+  def sqlite_restore_fkc
+    change_table :permission_template_accesses do |t|
+      t.references :permission_template, foreign_key: true
+    end
+    connection.execute("UPDATE permission_template_accesses SET permission_template_id=permission_template_id_old;")
+    remove_column :permission_template_accesses, :permission_template_id_old
+  end
+
+  def up
+    is_sql_lite = connection.adapter_name.downcase.starts_with?('sqlite')
+    sqlite_disable_fkc if is_sql_lite
+
+    # Separate admin_set_id into source_type/id
+    add_column :permission_templates, :source_type, :string
+    rename_column :permission_templates, :admin_set_id, :source_id
     Hyrax::PermissionTemplate.find_each do |permission_template|
       permission_template.source_type = 'admin_set'
-      permission_template.source_id = permission_template.admin_set_id
       permission_template.save!
     end
 
-    # If the db is sqlite, don't drop the column or you will get a FK error
-    # because of limitation in sqlite
-    remove_column :permission_templates, :admin_set_id, :string unless connection.adapter_name.downcase.starts_with?('sqlite')
+    sqlite_restore_fkc if is_sql_lite
+  end
+
+  def down
+    is_sql_lite = connection.adapter_name.downcase.starts_with?('sqlite')
+    sqlite_disable_fkc if is_sql_lite
+
+    # Recompose source_type/id into single field admin_set_id
+    remove_column :permission_templates, :source_type
+    rename_column :permission_templates, :source_id, :admin_set_id
+
+    sqlite_restore_fkc if is_sql_lite
   end
 end
