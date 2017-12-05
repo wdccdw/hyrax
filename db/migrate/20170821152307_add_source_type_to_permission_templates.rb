ba846c979371ce1b751f500fe9efea2c5a91e52b
Justin Gondron <jgondron@nd.edu>
2017-12-05 10:33:44 -0500

    #1580: Fixed an issue that broke the uk_permission_template_accesses index in sqlite.

--- db/migrate/20170821152307_add_source_type_to_permission_templates.rb
+++ db/migrate/20170821152307_add_source_type_to_permission_templates.rb
@@ -4,16 +4,26 @@ class AddSourceTypeToPermissionTemplates < ActiveRecord::Migration[4.2]
   # just to perform the upcoming column changes, which will violate the FKC.
   def sqlite_disable_fkc
     # Renaming the dependent column in sqlite actually recreates the table, silently dropping the fkc
-    rename_column :permission_template_accesses, :permission_template_id, :permission_template_id_old
+    rename_column :permission_template_accesses, :permission_template_id, :permission_template_id_temp
+    # Unfortunately, renaming the dependent column is also going to silently remove that field from
+    # this index. So we'll drop it for now and recreate when the migration is done
+    remove_index :permission_template_accesses, name: 'uk_permission_template_accesses'
   end
 
   # Recreate the permission_template_accesses table with the FKC.
   def sqlite_restore_fkc
+    # First recreate the original column 'permission_template_id' that references permission_template
     change_table :permission_template_accesses do |t|
       t.references :permission_template, foreign_key: true
     end
-    connection.execute("UPDATE permission_template_accesses SET permission_template_id=permission_template_id_old;")
-    remove_column :permission_template_accesses, :permission_template_id_old
+    # Copy the preserved data from the temp column and drop it
+    connection.execute("UPDATE permission_template_accesses SET permission_template_id=permission_template_id_temp;")
+    remove_column :permission_template_accesses, :permission_template_id_temp
+    # Recreate the index we lost. Pulled this from 20171117153051_add_unique_constraint_to_permission_template_accesses
+    add_index :permission_template_accesses,
+              [:permission_template_id, :agent_id, :agent_type, :access],
+              unique: true,
+              name: 'uk_permission_template_accesses'
   end
 
   def up
